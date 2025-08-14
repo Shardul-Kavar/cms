704b5b2eb31d171cca205e11b61263df2e4a1324
Shardul <shardulpatel010@gmail.com>
2025-08-15 01:04:09 +0530

    refactor: update Sequelize configuration to use connection string format

--- config/database.js
+++ config/database.js
@@ -1,14 +1,18 @@
 import { Sequelize } from "sequelize";
 import config from "./config.js";
 
-const { host, name, password, port, user } = config.db;
-
-const sequelizeConfig = new Sequelize(name, user, password, {
-  host,
-  port,
-  dialect: "postgres",
-  username: user,
-  password,
-});
+const sequelizeConfig = new Sequelize(
+  `postgresql://postgres.pzmwvrmhvhvimtooocmv:${config.db.password}@aws-1-ap-southeast-1.pooler.supabase.com:${config.db.port}/${config.db.name}`,
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  }
+);
 
 export default sequelizeConfig;
